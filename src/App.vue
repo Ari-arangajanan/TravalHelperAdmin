61a4dddf274c3d0004f2aa1305adedb50ed0dc2c
Ari arangajanan <arangajanan.nar@gmail.com>
2024-05-09 17:22:14 +0530

    learning

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to TH admin.js App"/>
+  <PAGES/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import PAGES from "./components/NewComponent.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    PAGES,
   }
 }
 </script>
